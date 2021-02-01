9e78274d11d00759fb222c2a26e57ec8368614b7
Chr1sDev <chris@chr1s.dev>
2021-01-31 21:00:13 -0600

    update click count and timer

--- aimtrainer.js
+++ aimtrainer.js
@@ -10,9 +10,9 @@ async function main() {
     const page = await browser.newPage();
     await page.setViewport({width: 1200, height: 720});
     await page.goto(url, { waitUntil: 'networkidle0' }); // wait until page load
-    await sleep(10000)
+    await sleep(9000)
 
-    for (let index = 0; index < 30; index++) {
+    for (let index = 0; index < 31; index++) {
         await page.click('.css-z6vxiy')
     }
 
