3ab0cca932ce582170f670ac27305be22e356ee6
Chr1sDev <chris@chr1s.dev>
2021-05-20 16:50:45 -0500

    make it take longer before starting, in case you're playing multiplayer

--- typeracer.js
+++ typeracer.js
@@ -10,7 +10,7 @@ async function main() {
     const page = await browser.newPage();
     await page.setViewport({width: 1200, height: 720});
     await page.goto(url, { waitUntil: 'networkidle0' }); // wait until page load
-    await sleep(3000)
+    await sleep(15000)
 
     await page.type('.txtInput',     await page.evaluate(async (page) => {
         var inputPanel = document.getElementsByClassName('inputPanel');
